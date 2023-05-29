a100ee78277d1c6996300c8444a9be54a2807573
amitshrestha2077 <132199045+amitshrestha2077@users.noreply.github.com>
2023-05-29 22:40:20 +0545

    my main file

---
+++ naruto.py
@@ -0,0 +1,33 @@
+import pygame
+pygame.init()
+
+win = pygame.display.set_mode((500, 500))
+
+x = 50
+y = 400
+width = 40
+height = 60
+speed = 5
+
+run = True
+
+while run:
+    pygame.time.delay(50)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            
+    keys = pygame.key.get_pressed()
+    
+    if keys[pygame.K_LEFT]:
+        x -= speed
+        
+    if keys[pygame.K_RIGHT]:
+        x += speed
+    
+    win.fill((0, 0, 0))
+    pygame.draw.rect(win, (255, 255, 250), (x, y, width, height))
+    pygame.display.update()
+
+pygame.quit()
