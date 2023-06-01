f3d26029b0f96073e89b93481757d4335bcda735
Amit Shrestha <amitshrestha2077@gmail.com>
2023-06-01 21:11:44 +0545

    new commit gare

--- naruto.py
+++ naruto.py
@@ -10,6 +10,8 @@ walkleft = [pygame.image.load('pics\\NR2.png'), pygame.image.load('pics\\NR3.png
 bg = pygame.image.load('pics\\bg.png')
 stan = pygame.image.load('pics\\Nstanding.png')
 
+clock = pygame.time.Clock()
+
 x = 50
 y = 400
 width = 40
@@ -19,14 +21,41 @@ isjump = False
 
 jumpheight = 10
 
+left = False
+right = False
+walkcount = 0
+
 run = True
 
 
 def redrawgamewindow():
+    global left
+    global right
+    global walkcount
     win.blit(bg, (0, 0))
+
+    if walkcount + 1 > 6:
+        walkcount = 0
+
+    if left and isjump == False:
+        win.blit(walkleft[walkcount // 2], (x, y))
+
+    elif right and isjump == False:
+        win.blit(walkRight[walkcount // 2], (x, y))
+
+    elif right and isjump:
+        win.blit(walkRight[2], (x, y))
+
+    elif left and isjump:
+        win.blit(walkleft[1], (x, y))
+
+    elif not (isjump):
+        win.blit(stan, (x, y))
 
 
 while run:
+
+    ###  jump
     pygame.time.delay(50)
 
     for event in pygame.event.get():
@@ -37,9 +66,13 @@ while run:
 
     if keys[pygame.K_LEFT] and x > speed:
         x -= speed
-
-    if keys[pygame.K_RIGHT] and x < 500 - width - speed:
+        left = True
+        right = False
+    # broundry
+    if keys[pygame.K_RIGHT] and x < 692 - width - speed:
         x += speed
+        right = False
+        left = True
 
     if isjump == False:
         if keys[pygame.K_SPACE]:
@@ -59,7 +92,6 @@ while run:
             isjump = False
             jumpheight = 10
 
-    pygame.draw.rect(win, (255, 255, 250), (x, y, width, height))
     pygame.display.update()
 
     redrawgamewindow()
